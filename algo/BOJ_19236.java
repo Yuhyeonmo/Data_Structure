8273f63fbc739d1b476cff81ea4f8b8fe7337da6
YuHyeonMo <kora1492@naver.com>
2020-07-21 08:37:13 +0900

    19236 Comment Additional

--- algo/BOJ_19236.java
+++ algo/BOJ_19236.java
@@ -3,30 +3,9 @@ import java.util.Collections;
 import java.util.Scanner;
 
 /*
- * 문제
-아기 상어가 성장해 청소년 상어가 되었다.
+ * 문제 풀이 
+ * sw test 당시와 비슷하게 품.
 
-4×4크기의 공간이 있고, 크기가 1×1인 정사각형 칸으로 나누어져 있다. 
-공간의 각 칸은 (x, y)와 같이 표현하며, x는 행의 번호, y는 열의 번호이다. 한 칸에는 물고기가 한 마리 존재한다. 각 물고기는 번호와 방향을 가지고 있다. 
-번호는 1보다 크거나 같고, 16보다 작거나 같은 자연수이며, 두 물고기가 같은 번호를 갖는 경우는 없다. 방향은 8가지 방향(상하좌우, 대각선) 중 하나이다.
-
-오늘은 청소년 상어가 이 공간에 들어가 물고기를 먹으려고 한다. 청소년 상어는 (0, 0)에 있는 물고기를 먹고, (0, 0)에 들어가게 된다. 상어의 방향은 (0, 0)에 있던 물고기의 방향과 같다. 이후 물고기가 이동한다.
-
-물고기는 번호가 작은 물고기부터 순서대로 이동한다. 
-물고기는 한 칸을 이동할 수 있고, 이동할 수 있는 칸은 빈 칸과 다른 물고기가 있는 칸, 이동할 수 없는 칸은 상어가 있거나, 공간의 경계를 넘는 칸이다. 
-각 물고기는 방향이 이동할 수 있는 칸을 향할 때까지 방향을 45도 반시계 회전시킨다. 만약, 이동할 수 있는 칸이 없으면 이동을 하지 않는다. 그 외의 경우에는 그 칸으로 이동을 한다. 
-물고기가 다른 물고기가 있는 칸으로 이동할 때는 서로의 위치를 바꾸는 방식으로 이동한다.
-
-물고기의 이동이 모두 끝나면 상어가 이동한다. 상어는 방향에 있는 칸으로 이동할 수 있는데, 한 번에 여러 개의 칸을 이동할 수 있다. 
-상어가 물고기가 있는 칸으로 이동했다면, 그 칸에 있는 물고기를 먹고, 그 물고기의 방향을 가지게 된다. 이동하는 중에 지나가는 칸에 있는 물고기는 먹지 않는다. 물고기가 없는 칸으로는 이동할 수 없다. 
-상어가 이동할 수 있는 칸이 없으면 공간에서 벗어나 집으로 간다. 상어가 이동한 후에는 다시 물고기가 이동하며, 이후 이 과정이 계속해서 반복된다.
-
-
-상어가 먹을 수 있는 물고기 번호의 합의 최댓값을 구해보자.
-
-입력
-첫째 줄부터 4개의 줄에 각 칸의 들어있는 물고기의 정보가 1번 행부터 순서대로 주어진다. 물고기의 정보는 두 정수 ai, bi로 이루어져 있고, ai는 물고기의 번호, bi는 방향을 의미한다. 
-방향 bi는 8보다 작거나 같은 자연수를 의미하고, 1부터 순서대로 ↑, ↖, ←, ↙, ↓, ↘, →, ↗ 를 의미한다.
  */
 public class BOJ_19236 {
 	
@@ -45,36 +24,47 @@ public class BOJ_19236 {
 				fishes.add(new Fishes(num, dir, i, j));
 			}	
 		}
+		// 초기값을 넣음 = 맨 처음 시작하는 위치는 잡히고 시작
 		ans = fishes.get(0).number;
+		// 숫자대로 정렬한번해주고,
 		Collections.sort(fishes);
+		// DFS 재귀 실행
 		run (fishes, 0,0,0,0);
+		
 		System.out.println(ans);
 	}
 	
 	public static void run(ArrayList<Fishes> f, int x, int y, int dir, int sum){
 		
+		// 먼저 먹을 수 있는지 확인
 		int res = isEat(f,x,y);
+		// -1, 먹을 수 없다. 재귀 종료
 		if(res==-1){
+			// ans 값보다 현재의 sum이 더 크면 ans값을 변경
 			if(ans<sum){
 				ans = sum;
 			}
 			return ;
 		}
-		
+		// 물고기 번호를 구해서 sum에 더해줌.
 		int n = f.get(res).number;
 		sum+=n;
+		// 방향을 해당 물고기의 x,y 및 방향으로 설정
 		dir = f.get(res).dir;
 		sharkX = f.get(res).x;
 		sharkY = f.get(res).y;
 		x = f.get(res).x;
 		y = f.get(res).y;
+		// 리스트에서 제거
 		f.remove(res);
 		
+		// 2. 물고기들의 이동
 		moveFish(f);
 		
 		for(int i=1;i<=3;i++){
 			
 			ArrayList<Fishes> copy = new ArrayList<Fishes>();
+			// 물고기 리스트가 재귀돌면서 변경되는 것을 막을려고 값을 복사한 copy 로 재귀 실행
 			copy = copyArr(f);
 		
 			int nsX = x + i*dx[dir];
@@ -83,6 +73,7 @@ public class BOJ_19236 {
 			run(copy, nsX, nsY, dir, sum);
 			
 		}
+		// 해당 위치에서 이동가능한 위치가 없을 경우를 대비해서 만듦
 		if(sum>ans) ans = sum;
 	}
 	public static ArrayList<Fishes> copyArr(ArrayList<Fishes> f){
@@ -102,7 +93,7 @@ public class BOJ_19236 {
 	}
 	public static void moveFish(ArrayList<Fishes> f){
 		
-		Collections.sort(f);
+		// 1번부터 순서대로 이동
 		for(int i=1;i<=16;i++){
 			Fishes fish = new Fishes(0,0,0,0);
 			for(Fishes ftmp : f){
@@ -111,18 +102,21 @@ public class BOJ_19236 {
 					break;
 				}
 			}
+			// 이미 잡힌 물고기인 경우
 			if(fish.number==0) continue;
 			int dir = fish.dir;
 			while(true){
-				
+				// 이동여부 확인 3
 				int r = isMove(f, i, fish.x, fish.y, dir);
-				
+				// 해당위치가 범위가 밖이거나 상어인 경우 방향을 회전시킴.
 				if(r==-1){
 					dir++;
+					// 8보다 커지면 맨처음으로
 					if(dir>8){
 						dir = 1;
 					}
 				}
+				// 빈칸 이동
 				else if(r==-2){
 					int nx = fish.x + dx[dir];
 					int ny = fish.y + dy[dir];
@@ -132,7 +126,7 @@ public class BOJ_19236 {
 					
 					break;
 				}
-				
+				// swap , 해당 위치의 물고기 번호와 방향만 재설정해서 바꾼다.
 				else {
 
 					int tmpNumber = fish.number;
@@ -158,18 +152,21 @@ public class BOJ_19236 {
 		int nx = x+dx[dir];
 		int ny = y+dy[dir];
 		
+		// 다음 위치가 이동할 수 없는 위치인 경우 -1
 		if(nx<0 || ny<0 || nx>=4 || ny>=4 || (nx==sharkX && ny==sharkY)) return -1;
 		
+		// 해당위치에 물고기가 있는지 찾음.
 		for(int i=0;i<f.size();i++){
 			if(f.get(i).number==idx) continue;
 			Fishes fish = f.get(i);
 			if(nx==fish.x && ny==fish.y) return i;
 		}
-		
+		// 이동할 수는 있지만, 해당 위치가 빈칸인 경우
 		return -2;
 	}
+	
 	public static int isEat(ArrayList<Fishes> f, int x, int y){
-		
+		// 해당 위치에 물고기가 있는지 확인, 있으면 해당 인덱스를 리턴하도록
 		for(int i=0;i<f.size();i++){
 			
 			Fishes fish = f.get(i);
@@ -177,7 +174,7 @@ public class BOJ_19236 {
 			
 		}
 		
-		
+		// 없으면 -1
 		return -1;
 	}
 	
